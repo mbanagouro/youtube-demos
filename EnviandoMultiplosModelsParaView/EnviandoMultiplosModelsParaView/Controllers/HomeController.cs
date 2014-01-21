7d1009dabd70a65352f1bb5daa8ba3e5da291f29
Michel Banagouro <mbanagouro@outlook.com>
2014-01-21 01:23:47 -0200

    ASP .NET MVC - Enviando multiplos models para view

---
+++ EnviandoMultiplosModelsParaView/EnviandoMultiplosModelsParaView/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using EnviandoMultiplosModelsParaView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnviandoMultiplosModelsParaView.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            var model = new HomeIndexModel();
+
+            model.Produto = ObterProduto();
+            model.Categorias = ObterCategorias();
+            model.Marcas = ObterMarcas();
+
+            return View(model);
+        }
+
+        #region Operações
+
+        Produto ObterProduto()
+        {
+            return new Produto
+            {
+                Id = 1,
+                Nome = "Bola de futebol"
+            };
+        }
+
+        IEnumerable<Marca> ObterMarcas()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                yield return new Marca
+                {
+                    Id = i,
+                    Nome = "Marca " + i
+                };
+            }
+        }
+
+        IEnumerable<Categoria> ObterCategorias()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                yield return new Categoria
+                {
+                    Id = i,
+                    Nome = "Categoria " + i
+                };
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ EnviandoMultiplosModelsParaView/EnviandoMultiplosModelsParaView/Models/HomeIndexModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnviandoMultiplosModelsParaView.Models
+{
+    public class HomeIndexModel
+    {
+        public Produto Produto { get; set; }
+
+        public IEnumerable<Categoria> Categorias { get; set; }
+
+        public IEnumerable<Marca> Marcas { get; set; }
+    }
+}
